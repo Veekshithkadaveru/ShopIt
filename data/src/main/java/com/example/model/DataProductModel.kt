896091edc8af1e69989bb7ad356bc45efc8ad94e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-30 19:16:49 +0530

    Add DataProductModel with serialization support and conversion to Product

--- data/src/main/java/com/example/model/DataProductModel.kt
+++ data/src/main/java/com/example/model/DataProductModel.kt
@@ -1,4 +1,19 @@
 package com.example.model
 
-class DataProductModel {
+import com.example.domain.model.Product
+import kotlinx.serialization.Serializable
+
+@Serializable
+class DataProductModel(
+    val id: Long,
+    val title: String,
+    val price: Double,
+    val category: String,
+    val description: String,
+    val image: String
+) {
+
+    fun toProduct() = Product(
+        id, title, price, category, description, image
+    )
 }
